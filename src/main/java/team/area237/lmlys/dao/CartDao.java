8980aea939d91f453c9f4432d2fb8092691e951c
latlajlat <latlajlat@gmail.com>
2019-07-02 17:49:24 +0800

    shoppingService初步完成

---
+++ src/main/java/team/area237/lmlys/dao/CartDao.java
@@ -0,0 +1,21 @@
+package team.area237.lmlys.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+import team.area237.lmlys.model.entity.Cart;
+
+import java.util.List;
+
+@Mapper
+@Component
+public interface CartDao {
+    //购物车的商品ID数量
+    int amountSelectByUsername(@Param("username")String username);
+    //获取购物车内商品的信息Cart:String goodsId,int count
+    List<Cart> InfoSelectByUsername(@Param("username")String username);
+    //上传当前用户的购物车信息
+    int replaceByUsername(@Param("username")String username,@Param("carts") Cart[] carts);
+    //添加新商品
+    int insertByUsername(@Param("username")String username,@Param("goods_id")int id,@Param("count")int count);
+}
--- src/main/java/team/area237/lmlys/service/ShoppingService.java
+++ src/main/java/team/area237/lmlys/service/ShoppingService.java
@@ -1,12 +1,13 @@
 package team.area237.lmlys.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import team.area237.lmlys.model.request.NewGoodsToCartRequest;
 import team.area237.lmlys.model.request.UpdateCartResquest;
 import team.area237.lmlys.model.response.ShoppingCartCountResponse;
 import team.area237.lmlys.model.response.ShoppingCartResponse;
 import team.area237.lmlys.model.response.UploadCartResponse;
-
+@Service
 @Component
 public interface ShoppingService {
     //返回用户购物车中的商品数量
