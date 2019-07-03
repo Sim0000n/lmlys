c19271fddf18536f8ce99de31dad3cb685349650
latlajlat <latlajlat@gmail.com>
2019-07-04 01:58:20 +0800

    ManagerService半完成

--- src/main/java/team/area237/lmlys/dao/RegisterDao.java
+++ src/main/java/team/area237/lmlys/dao/RegisterDao.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 import team.area237.lmlys.model.request.RegisterRequest;
 import team.area237.lmlys.model.request.UploadUserAddressRequest;
+import team.area237.lmlys.model.response.GetOrderResponse;
 import team.area237.lmlys.model.response.GetUserAddressResponse;
 import team.area237.lmlys.model.response.UserDataResponse;
 
@@ -15,10 +16,8 @@ import java.util.List;
 public interface RegisterDao {
     String selectByUsername(@Param("username")String username);
     int addUser(@Param("username")String username, @Param("password")String password);
-
     //获得用户的email和phone
     UserDataResponse dataSelectByUsername(@Param("username")String username);
-
     //上传用户phone
     int insertPhoneByUsername(@Param("username")String username,@Param("phone")String phone);
     //上传用户email
@@ -27,13 +26,18 @@ public interface RegisterDao {
     int insertBothByUsername(@Param("username")String username,@Param("phone")String phone,@Param("email")String email);
     //获取用户地址信息
     GetUserAddressResponse selectAddressByUsername(@Param("username")String username);
-
     //上传用户地址信息
-    int insertAddressByUsername(@Param("usersname")String username, @Param("address")UploadUserAddressRequest uploadUserAddressRequest);
-
-
+    int insertAddressByUsername(@Param("username")String username, @Param("address")UploadUserAddressRequest uploadUserAddressRequest);
     //只返回省名
     List<String> selectProvince();
     //返回市名
     List<String> citySelectByProvince(@Param("province")String province);
-    }
+
+
+    //购物车转为订单
+    int cartToOrder(@Param("username")String username);
+    //返回用户订单号，按时间排序,新的在前，旧的在后
+    List<Integer> selectOrderByUsername(@Param("username")String username);
+    //根据订单ID返回商品ID，标题，数量，订单状态，状态最后修改时间
+    GetOrderResponse selectOrderById(@Param("id")int id);
+}
